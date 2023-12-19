245f8366b76c4823527e4ab1db746067b2a31695
Peanut <138329745+pe4nutlord@users.noreply.github.com>
2023-12-19 16:00:06 -0500

    Update TheyLeftVC.py

--- TheyLeftVC.py
+++ TheyLeftVC.py
@@ -1,18 +1,13 @@
-# Install discord.py
-# Command: pip install discord.py
-
 import discord
 import asyncio
 
 intents = discord.Intents.default()
 intents.voice_states = True
 
-# Replace with the IDs of the people you want to track
 TARGET_USER_IDS = [704860044268929064]
-TARGET_VC_ID = 1186118770545147927  # Replace with the ID of the voice channel to track
-TARGET_TEXT_CHANNEL_ID = 1186118770545147926  # Replace with the ID of the text channel to send the message
-BOT_TOKEN = 'YOUR_BOT_TOKEN'  # Replace with your actual bot token
-
+TARGET_VC_ID = 1186118770545147927
+TARGET_TEXT_CHANNEL_ID = 1186118770545147926
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
 client = discord.Client(intents=intents)
 
 @client.event
@@ -40,13 +35,10 @@ async def on_voice_state_update(member, before, after):
     if member.id in TARGET_USER_IDS and before.channel and before.channel.id == TARGET_VC_ID and not after.channel:
         target_text_channel = client.get_channel(TARGET_TEXT_CHANNEL_ID)
 
-        # Send the ping message
         await target_text_channel.send(f'{member.mention}')
 
-        # Introduce a delay (adjust the time as needed)
         await asyncio.sleep(-1)
 
-        # Send the main message
         await target_text_channel.send('https://tenor.com/view/stranger-things-stranger-things-vc-discord-vc-leaving-vc-gif-26345043')
 
 client.run(BOT_TOKEN)
